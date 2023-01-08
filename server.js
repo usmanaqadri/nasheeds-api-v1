51fa8a1e32129164799d67884c5856e7f86451c1
usmanaqadri <usmanaqadri@gmail.com>
2023-01-08 08:58:05 -0500

    made it more dev prod friendly

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const FRONTEND_ORIGIN =
-  process.env.FRONTEND_ORIGIN || "http://localhost:3000/nasheeds-all-night";
+  process.env.NODE_ENV === "dev"
+    ? "http://localhost:3000"
+    : process.env.FRONTEND_ORIGIN;
 
 //middleware
 app.use(express.json());
