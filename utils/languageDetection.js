b38f15db8c20562e49c3cb18f4031bb3a28b1dec
usmanaqadri <usmanaqadri@gmail.com>
2025-07-04 20:13:56 -0400

    transliteration before translation and some pdfs wouldnt export

--- utils/languageDetection.js
+++ utils/languageDetection.js
@@ -1,14 +1,87 @@
-const reverseWords = (text) => {
-  return text.split(" ").reverse().join(" ");
+const reverseWords = (text, numberOfSpaces) => {
+  const spaces = " ".repeat(numberOfSpaces);
+  return text.split(" ").reverse().join(spaces);
 };
 
 const isUrdu = (text) => {
-  const urduChars = ["ے", "ٹ", "ں", "ڈ", "ڑ", "ؤ", "ۂ", "ۓ"];
+  const urduChars = [
+    "ا",
+    "ب",
+    "پ",
+    "ت",
+    "ٹ",
+    "ث",
+    "ج",
+    "چ",
+    "ح",
+    "خ",
+    "د",
+    "ڈ",
+    "ذ",
+    "ر",
+    "ڑ",
+    "ز",
+    "ژ",
+    "س",
+    "ش",
+    "ص",
+    "ض",
+    "ط",
+    "ظ",
+    "ع",
+    "غ",
+    "ف",
+    "ق",
+    "ک",
+    "گ",
+    "ل",
+    "م",
+    "ن",
+    "ں",
+    "و",
+    "ؤ",
+    "ہ",
+    "ۂ",
+    "ء",
+    "ی",
+    "ے",
+    "ۓ",
+    "ء",
+    "ػ",
+    "ؼ",
+    "ؽ",
+    "ؾ",
+    "ؿ",
+    "ٲ",
+    "ٳ",
+    "ٴ",
+    "ٵ",
+    "ٹ",
+    "پ",
+    "چ",
+    "ژ",
+    "ک",
+    "گ",
+    "ں",
+    "ہ",
+    "ۂ",
+    "ۓ",
+    "ٰ",
+    "ں",
+    "َ",
+    "ُ",
+    "ِ",
+    "ّ",
+    "ْ",
+    "ٓ",
+    "ٔ",
+    "ٕ",
+  ];
   return urduChars.some((char) => text.includes(char));
 };
 
-const detectLanguage = (lines, linesToCheck = 5) => {
-  const checkCount = Math.min(linesToCheck, lines.length);
+const detectLanguage = (lines) => {
+  const checkCount = lines.length;
   for (let i = 0; i < checkCount; i++) {
     if (isUrdu(lines[i])) {
       return "urdu";
